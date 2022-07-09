c7edc4c3f2403a387efd9d04f1cb80fdcdfd3a4a
anupamKhatiwada01 <anupamkhatiwada01@gmail.com>
2022-07-09 18:30:01 +0000

    .

--- chapterOne.js
+++ chapterOne.js
@@ -106,12 +106,24 @@ let inc = x => x+1;
 let dec = x => x-1;
 
 // Below is a recursive function
-let plus = (a,b) => a===0?b:inc(plus(dec(a),b));
+// let plus = (a,b) => a===0?b:inc(plus(dec(a),b));
 // Let's write the iterative version
-let plusIter = (a,b) => a==0?b:plus(dec(a),inc(b));
+let plusIter = (a,b) => a==0?b:plusIter(dec(a),inc(b));
 
+// Ackermann function
+let A = (x,y) => y==0?0:x==0?2*y:y===1?2:A(x-1,A(x,y-1));
 
+// Fibonacci analysis
+// Regular fibonacci (unoptimized) uses a number of steps that grows exponentially with input.
+// On the other hand space required grows only linearly with input because we need to keep track of only the nodes above us in the tree at any point in the computation. In general, the number of steps in a tree recursive process will be proportiional
+// to the number of nodes in the tree while the space required will be proportional to the maximum depth of the tree.
 
+let fiboRecursive = n => n<2?n:fiboRecursive(n-1)+fiboRecursive(n-2);
+
+let fiboIter = (n,counter=0,sum=0) => n<2?sum:fiboIter(--n, counter++, sum+counter);
+// Both the above two implementations of fibonacci are wrong. Go through them and correct them.
+console.log(fiboIter(3));
+console.log(fiboRecursive(5));
 
 
 
